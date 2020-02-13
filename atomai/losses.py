d1dd385a9c0b2f56653542c9e8b29242a9e7e18d
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2020-02-13 05:15:06 +0000

    Add focal loss

--- atomai/losses.py
+++ atomai/losses.py
@@ -1,10 +1,41 @@
-# TODO Add focal loss
 """
 Custom pytorch loss functions
 """
 
 import torch
 import torch.nn.functional as F
+
+
+class focal_loss(torch.nn.Module):
+    """
+    Loss function for classification tasks  with
+    large data imbalance. Focal loss (FL) is define as:
+    FL(p_t) = -alpha*((1-p_t)^gamma))*log(p_t),
+    where p_t is a cross-entropy loss for binary classification.
+    For more details, see https://arxiv.org/abs/1708.02002.
+    """
+    def __init__(self, alpha=0.5, gamma=2, with_logits=True):
+        """
+        Args:
+            alpha (float): "balance" coefficient,
+            gamma (float): "focusing" parameter (>=0),
+            with_logits (bool): indicates if the sigmoid operation was applied
+            at the end of a neural network's forward path.
+        """
+        super(focal_loss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.logits = with_logits
+
+    def forward(self, images, labels):
+        """Calculate focal loss"""
+        if self.logits:
+            CE_loss = F.binary_cross_entropy_with_logits(images, labels)
+        else:
+            CE_loss = F.binary_cross_entropy(images, labels)
+        pt = torch.exp(-CE_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * CE_loss
+        return F_loss
 
 
 class dice_loss(torch.nn.Module):
