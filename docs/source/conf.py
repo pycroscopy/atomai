38a90c985901a6d90a3c8bdc95a422ef2c58aa28
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2020-03-08 18:41:42 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,12 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
+import mock
+
+MOCK_MODULES = ['torch']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # -- Project information -----------------------------------------------------
 
 project = 'AtomAI'
