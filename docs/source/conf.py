df3cb79cd3a4cb93ec80f46fe5d239cd4835c1db
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2020-03-08 18:46:18 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 import mock
 
-MOCK_MODULES = ['torch']
+MOCK_MODULES = ['torch', 'torch.nn', 'torch.nn.functional']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
