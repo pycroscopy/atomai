281442e00d7963b517d01f464f1f4dd24a6500a4
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2020-06-25 01:52:53 +0000

    Add standard VAE

--- atomai/experimental/vae.py
+++ atomai/experimental/vae.py
@@ -115,10 +115,64 @@ class rDecoderNet(nn.Module):
         return F.softplus(out)
 
 
+class DecoderNet(nn.Module):
+    """
+    Spatial decoder network
+    (based on https://arxiv.org/abs/1909.11663)
+
+    Args:
+
+    latent_dim: number of latent dimensions associated with images content
+    num_layers: number of fully connected layers
+    hidden_dim: number of neurons in each fully connected layer
+    out_dim: output image dimensions (height and width)
+    mlp: using a simple multi-layer perceptron instead of convolutional layers (Default: False)
+    """
+    def __init__(self,
+                 latent_dim: int,
+                 num_layers: int,
+                 hidden_dim: int,
+                 out_dim: Tuple[int, int],
+                 mlp: bool = False,) -> None:
+        """
+        Initializes network parameters
+        """
+        super(DecoderNet, self).__init__()
+        self.mlp = mlp
+        flatten_dim = hidden_dim * out_dim[0] * out_dim[1]
+        self.fc_linear = nn.Linear(
+                latent_dim, flatten_dim, bias=False)
+        if not self.mlp:
+            self.reshape_ = (hidden_dim, out_dim[0], out_dim[1])
+            self.decoder = models.conv2dblock(
+                num_layers, hidden_dim, hidden_dim, lrelu_a=0.1)
+            self.out = nn.Conv2d(hidden_dim, 1, 1, 1, 0)
+        else:
+            self.fc_linear = nn.Sequential(self.fc_linear, nn.Tanh())
+            decoder = []
+            for i in range(num_layers):
+                hidden_dim_ = flatten_dim if i == 0 else hidden_dim
+                decoder.extend([nn.Linear(hidden_dim_, hidden_dim), nn.Tanh()])
+            self.decoder = nn.Sequential(*decoder)
+            self.out = nn.Linear(hidden_dim, out_dim[0] * out_dim[1])
+        self.out_dim = (1, *out_dim)
+
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass
+        """
+        h = self.fc_linear(z)
+        if not self.mlp:
+            h = h.reshape(-1, *self.reshape_)
+        h = self.decoder(h)
+        h = self.out(h)
+        h = h.reshape(-1, *self.out_dim)
+        return h.squeeze(1)
+
+
 class EncoderDecoder:
     """
     General class for Encoder-Decoder type of deep latent variable models
-    indepentent of rotations and translations
 
     Args:
         im_dim: xy planar dimensions (height, width) of input images
@@ -133,7 +187,7 @@ class EncoderDecoder:
     def __init__(self,
                  im_dim: Tuple[int, int],
                  latent_dim: int,
-                 conv_encoder: bool = False,
+                 coord: bool = True,
                  seed: int = 0,
                  **kwargs: Dict) -> None:
 
@@ -147,17 +201,27 @@ class EncoderDecoder:
         np.random.seed(seed)
 
         self.im_dim = im_dim
-        self.z_dim = latent_dim + 3
-        mlp = not conv_encoder
+        self.z_dim = latent_dim
+        if coord:
+            self.z_dim = latent_dim + 3
+        mlp_e = not kwargs.get("conv_encoder", False)
+        if not coord:
+            mlp_d = not kwargs.get("conv_decoder", False)
         numlayers_e = kwargs.get("numlayers_encoder", 2)
         numlayers_d = kwargs.get("numlayers_decoder", 2)
         numhidden_e = kwargs.get("numhidden_encoder", 128)
         numhidden_d = kwargs.get("numhidden_encoder", 128)
 
-        self.decoder_net = rDecoderNet(
-            latent_dim, numlayers_d, numhidden_d, self.im_dim)
+        if not coord:
+            self.decoder_net = DecoderNet(
+                latent_dim, numlayers_d, numhidden_d, self.im_dim, mlp_d)
+        else:
+            self.decoder_net = rDecoderNet(
+                latent_dim, numlayers_d, numhidden_d, self.im_dim)
         self.encoder_net = EncoderNet(
-            self.im_dim, self.z_dim, numlayers_e, numhidden_e, mlp)
+            self.im_dim, self.z_dim, numlayers_e, numhidden_e, mlp_e)
+
+        self.coord = coord
 
     def load_weights(self, filepath):
         weights = torch.load(filepath)
@@ -186,7 +250,7 @@ class EncoderDecoder:
         def inference() -> np.ndarray:
             with torch.no_grad():
                 z_mean, z_sd = self.encoder_net(x_i)
-            return z_mean.cpu().numpy(), z_sd.cpu().numpy()
+            return z_mean.cpu().numpy(), torch.exp(z_sd.cpu()).numpy()
 
         if isinstance(x_test, np.ndarray):
             x_test = torch.from_numpy(x_test).float()
@@ -226,18 +290,23 @@ class EncoderDecoder:
             z_sample = torch.from_numpy(z_sample).float()
         if len(z_sample.size()) == 1:
             z_sample = z_sample[None, ...]
-        xx = torch.linspace(-1, 1, n)
-        yy = torch.linspace(1, -1, m)
-        x0, x1 = torch.meshgrid(xx, yy)
-        x_coord = torch.stack([x0.T.flatten(), x1.T.flatten()], axis=1)
-        x_coord = x_coord.expand(z_sample.size(0), *x_coord.size())
+        if self.coord:
+            xx = torch.linspace(-1, 1, n)
+            yy = torch.linspace(1, -1, m)
+            x0, x1 = torch.meshgrid(xx, yy)
+            x_coord = torch.stack([x0.T.flatten(), x1.T.flatten()], axis=1)
+            x_coord = x_coord.expand(z_sample.size(0), *x_coord.size())
+            if torch.cuda.is_available():
+                x_coord = x_coord.cuda()
         if torch.cuda.is_available():
             z_sample = z_sample.cuda()
-            x_coord = x_coord.cuda()
             self.decoder_net.cuda()
         self.decoder_net.eval()
         with torch.no_grad():
-            x_decoded = self.decoder_net(x_coord, z_sample)
+            if self.coord:
+                x_decoded = self.decoder_net(x_coord, z_sample)
+            else:
+                x_decoded = self.decoder_net(z_sample)
         imdec = x_decoded.cpu().numpy()
         return imdec
 
@@ -263,7 +332,7 @@ class EncoderDecoder:
         for i in range(num_samples):
             z_sample = ndist.rsample()
             z_sample = z_sample.view(1, -1)
-            decoded_all.append(self.decode_(z_sample))  # Works only for regular VAE
+            decoded_all.append(self.decode_(z_sample))
         decoded_all = np.concatenate(decoded_all, axis=0)
         return decoded_all
 
@@ -323,11 +392,11 @@ class rVAE(EncoderDecoder):
                  training_cycles: int = 300,
                  minibatch_size: int = 200,
                  test_size: float = 0.15,
-                 conv_encoder: bool = False,
                  seed: int = 0,
                  **kwargs: Dict) -> None:
         dim = imstack.shape[1:3]
-        super(rVAE, self).__init__(dim, latent_dim, conv_encoder, seed, **kwargs)
+        coord = True
+        super(rVAE, self).__init__(dim, latent_dim, coord, seed, **kwargs)
 
         if torch.cuda.is_available:
             torch.cuda.empty_cache()
@@ -438,7 +507,156 @@ class rVAE(EncoderDecoder):
 
     def evaluate_model(self):
         """
-        Evaluates model on the test data
+        Evaluates model on test data
+        """
+        self.decoder_net.eval()
+        self.encoder_net.eval()
+        c = 0
+        elbo_epoch_test = 0
+        for x, in self.test_iterator:
+            b = x.size(0)
+            elbo = self.step(x, mode="eval")
+            elbo = elbo.item()
+            c += b
+            delta = b * (elbo - elbo_epoch_test)
+            elbo_epoch_test += delta / c
+        return elbo_epoch_test
+
+    def run(self):
+        """
+        Trains rVAE model
+        """
+        for e in range(self.training_cycles):
+            elbo_epoch = self.train_epoch()
+            elbo_epoch_test = self.evaluate_model()
+            template = 'Epoch: {}/{}, Training loss: {:.4f}, Test loss: {:.4f}'
+            print(template.format(e+1, self.training_cycles,
+                  -elbo_epoch, -elbo_epoch_test))
+        return
+
+
+class VAE(EncoderDecoder):
+    """
+    Implements a standard Variational Autoencoder (VAE)
+
+    Args:
+        imstack: 3D stack of training images (n x w x h)
+        latent_dim: number of VAE latent dimensions associated with image content
+        training_cycles: number of training 'epochs' (Default: 300)
+        minibatch_size: size of training batch for each training epoch (Default: 200)
+        test_size: proportion of the dataset for model evaluation (Default: 0.15)
+        seed: seed for torch and numpy (pseudo-)random numbers generators
+        **conv_encoder: use convolutional layers in encoder
+        **conv_decoder: use convolutional layers in decoder
+        **numlayers_encoder: number of layers in encoder (Default: 2)
+        **numlayers_decoder: number of layers in decoder (Default: 2)
+        **numhidden_encoder: number of hidden units OR conv filters in encoder (Default: 128)
+        **numhidden_decoder: number of hidden units OR conv filters in decoder (Default: 128)
+    """
+    def __init__(self,
+                 imstack: np.ndarray,
+                 latent_dim: int = 2,
+                 training_cycles: int = 300,
+                 minibatch_size: int = 200,
+                 test_size: float = 0.15,
+                 seed: int = 0,
+                 **kwargs: Dict) -> None:
+        dim = imstack.shape[1:3]
+        coord = False
+        super(VAE, self).__init__(dim, latent_dim, coord, seed, **kwargs)
+
+        if torch.cuda.is_available:
+            torch.cuda.empty_cache()
+        torch.manual_seed(seed)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed(seed)
+            torch.backends.cudnn.deterministic = True
+            torch.backends.cudnn.benchmark = False
+        np.random.seed(seed)
+
+        imstack_train, imstack_test = train_test_split(
+            imstack, test_size=test_size, shuffle=True, random_state=seed)
+
+        X_train = torch.from_numpy(imstack_train).float()
+        X_test = torch.from_numpy(imstack_test).float()
+
+        self.im_dim = imstack.shape[1:3]
+
+        if torch.cuda.is_available():
+            X_train = X_train.cuda()
+            X_test = X_test.cuda()
+
+        data_train = torch.utils.data.TensorDataset(X_train)
+        data_test = torch.utils.data.TensorDataset(X_test)
+        self.train_iterator = torch.utils.data.DataLoader(
+            data_train, batch_size=minibatch_size, shuffle=True)
+        self.test_iterator = torch.utils.data.DataLoader(
+            data_test, batch_size=minibatch_size)
+
+        if torch.cuda.is_available():
+            self.decoder_net.cuda()
+            self.encoder_net.cuda()
+
+        params = list(self.decoder_net.parameters()) +\
+            list(self.encoder_net.parameters())
+        self.optim = torch.optim.Adam(params, lr=1e-4)
+
+        self.training_cycles = training_cycles
+
+    def step(self,
+             x: torch.Tensor,
+             mode: str = "train") -> torch.Tensor:
+        """
+        Single training/test step
+        """
+        batch_dim = x.size(0)
+        if torch.cuda.is_available():
+            x = x.cuda()
+        if mode == "eval":
+            with torch.no_grad():
+                z_mean, z_logsd = self.encoder_net(x)
+        else:
+            z_mean, z_logsd = self.encoder_net(x)
+        z_sd = torch.exp(z_logsd)
+        z_dim = z_mean.size(1)
+        eps = x.new(batch_dim, z_dim).normal_()
+        z = z_mean + z_sd * eps
+
+        if mode == "eval":
+            with torch.no_grad():
+                x_reconstr = self.decoder_net(z)
+        else:
+            x_reconstr = self.decoder_net(z)
+        reconstr_error = -0.5 * torch.sum(
+            (x_reconstr.view(batch_dim, -1) - x.view(batch_dim, -1))**2, 1).mean()
+        kl_z = -z_logsd + 0.5 * z_sd**2 + 0.5 * z_mean**2 - 0.5
+        kl_z = torch.sum(kl_z, 1).mean()
+        return reconstr_error - kl_z
+
+    def train_epoch(self):
+        """
+        Trains a single epoch
+        """
+        self.decoder_net.train()
+        self.encoder_net.train()
+        c = 0
+        elbo_epoch = 0
+        for x, in self.train_iterator:
+            b = x.size(0)
+            elbo = self.step(x)
+            loss = -elbo
+            loss.backward()
+            self.optim.step()
+            self.optim.zero_grad()
+            elbo = elbo.item()
+            c += b
+            delta = b * (elbo - elbo_epoch)
+            elbo_epoch += delta / c
+        return elbo_epoch
+
+    def evaluate_model(self):
+        """
+        Evaluates model on test data
         """
         self.decoder_net.eval()
         self.encoder_net.eval()
