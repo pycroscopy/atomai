3224d8d4cfe32a1aa93aeb96ce1c230356a4acc1
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2020-08-20 05:29:24 +0000

    Update import statements

--- atomai/nets/__init__.py
+++ atomai/nets/__init__.py
@@ -1,4 +1,8 @@
-from .blocks import *
-from .fcnn import *
-from .ed import *
-from .loaders import *
+from .blocks import conv2dblock, upsample_block, dilated_block
+from .fcnn import dilUnet, dilnet
+from .ed import EncoderNet, DecoderNet, rDecoderNet, coord_latent
+from .loaders import load_model, load_ensemble
+
+__all__ = ['conv2dblock', 'upsample_block', 'dilated_block',
+           'dilUnet', 'dilnet', 'EncoderNet', 'DecoderNet', 'rDecoderNet',
+           'coord_latent', 'load_model', 'load_ensemble']
