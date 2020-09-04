7d0c1e8c13a8db1e86782361fbc4f184852adb4e
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2020-09-04 02:18:07 -0400

    Update homepage url
    The PyPI still has the old one but will get automatically updated in the next release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         description='Deep and machine learning for atom-resolved data',
         long_description=open(os.path.join(module_dir, 'README.md')).read(),
         long_description_content_type='text/markdown',
-        url='https://github.com/ziatdinovmax/atomai',
+        url='https://github.com/pycroscopy/atomai/',
         author='Maxim Ziatdinov',
         author_email='maxim.ziatdinov@ai4microcopy.com',
         license='MIT license',
