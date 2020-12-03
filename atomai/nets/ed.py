206447c9b4168245bf0ec4ee0b533fea046f12fe
ziatdinovmax <ziatdinovmax@gmail.com>
2020-12-03 07:38:16 +0000

    Move VAE encoder & decoder modules to ed.py.
    Also, split EncoderNet into convolutional and fully connected EncoderNet
    Same for DecoderNet

--- atomai/nets/ed.py
+++ atomai/nets/ed.py
@@ -2,7 +2,7 @@
 imspec.py
 =========
 
-Encoder and decoder modules for im2spec and spec2im models
+Encoder and decoder modules for VAE/VED and im2spec/spec2im models
 
 Created by Maxim Ziatdinov (email: maxim.ziatdinov@ai4microscopy.com)
 
@@ -157,6 +157,333 @@ class signal_ed(nn.Module):
         """
         x = self.encode(x)
         return self.decode(x)
+
+
+class convEncoderNet(nn.Module):
+    """
+    Convolutional rncoder/inference network (for variational autoencoder)
+
+    Args:
+        dim (tuple):
+            Input dimensions.
+            For images, it is (height, width) or (height, width, channels).
+            For spectra, it is (length,)
+        latent_dim (int):
+            number of latent dimensions
+            (the first 3 latent dimensions are angle & translations by default)
+        num_layers (int):
+            number of NN layers
+        hidden_dim (int):
+            number of neurons in each fully connnected layer (for mlp=True)
+            or number of filters in each convolutional layer (for mlp=False)
+    """
+    def __init__(self,
+                 in_dim: Tuple[int],
+                 latent_dim: int = 2,
+                 num_layers: int = 2,
+                 hidden_dim: int = 32,
+                 **kwargs: float
+                 ) -> None:
+        """
+        Initializes network parameters
+        """
+        super(convEncoderNet, self).__init__()
+        if len(in_dim) not in (1, 2, 3):
+            raise ValueError(
+                "The input dimensions must be (length,) for 1D data and " +
+                "(height, width) or (height, width, channel) for 2D data")
+        dim = 2 if len(in_dim) > 1 else 1
+        c = in_dim[-1] if len(in_dim) > 2 else 1
+        self.conv = ConvBlock(
+            dim, num_layers, c, hidden_dim,
+            lrelu_a=kwargs.get("lrelu_a", 0.1))
+        self.reshape_ = hidden_dim * np.product(in_dim[:2])
+        self.fc11 = nn.Linear(self.reshape_, latent_dim)
+        self.fc12 = nn.Linear(self.reshape_, latent_dim)
+        
+    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor]:
+        """
+        Forward pass
+
+        Args:
+            x: Input tensor with channel (> 1) as the last dimension
+        """
+        x = x.unsqueeze(1) if x.ndim in (2, 3) else x.permute(0, -1, 1, 2)
+        x = self.conv(x)
+        x = x.reshape(-1, self.reshape_)
+        z_mu = self.fc11(x)
+        z_logstd = self.fc12(x)
+        return z_mu, z_logstd
+
+
+class fcEncoderNet(nn.Module):
+    """
+    Encoder/inference network (for variational autoencoder)
+
+    Args:
+        dim (tuple):
+            Input dimensions.
+            For images, it is (height, width) or (height, width, channels).
+            For spectra, it is (length,)
+        latent_dim (int):
+            number of latent dimensions
+            (the first 3 latent dimensions are angle & translations by default)
+        num_layers (int):
+            number of NN layers
+        hidden_dim (int):
+            number of neurons in each fully connnected layer (for mlp=True)
+            or number of filters in each convolutional layer (for mlp=False)
+    """
+    def __init__(self,
+                 in_dim: Tuple[int],
+                 latent_dim: int = 2,
+                 num_layers: int = 2,
+                 hidden_dim: int = 32,
+                 ) -> None:
+        """
+        Initializes network parameters
+        """
+        super(fcEncoderNet, self).__init__()
+        dense = []
+        for i in range(num_layers):
+            input_dim = np.product(in_dim) if i == 0 else hidden_dim
+            dense.extend([nn.Linear(input_dim, hidden_dim), nn.Tanh()])
+        self.dense = nn.Sequential(*dense)
+        self.reshape_ = hidden_dim
+        self.fc11 = nn.Linear(self.reshape_, latent_dim)
+        self.fc12 = nn.Linear(self.reshape_, latent_dim)
+
+    def forward(self, x: torch.Tensor):
+        """
+        Forward pass
+        """
+        x = x.reshape(-1, np.product(x.size()[1:]))
+        x = self.dense(x)
+        x = x.reshape(-1, self.reshape_)
+        z_mu = self.fc11(x)
+        z_logstd = self.fc12(x)
+        return z_mu, z_logstd
+
+
+class convDecoderNet(nn.Module):
+    """
+    Convolutional decoder network (for variational autoencoder)
+
+    Args:
+        out_dim (tuple):
+            Output dimensions.
+            For images, it is (height, width) or (height, width, channels).
+            For spectra, it is (length,)
+        latent_dim (int):
+            number of latent dimensions associated with images content
+        num_layers (int):
+            number of fully connected layers
+        hidden_dim (int):
+            number of neurons in each fully connected layer
+    """
+    def __init__(self,
+                 out_dim: Tuple[int],
+                 latent_dim: int,
+                 num_layers: int = 2,
+                 hidden_dim: int = 32,
+                 num_classes: int = 0,
+                 **kwargs: float) -> None:
+        """
+        Initializes network parameters
+        """
+        super(convDecoderNet, self).__init__()
+        if len(out_dim) not in (1, 2, 3):
+            raise ValueError(
+                "The output dimensions must be (length,) for 1D data and " +
+                "(height, width) or (height, width, channel) for 2D data")
+        dim = 2 if len(out_dim) > 1 else 1
+        c = out_dim[-1] if len(out_dim) > 2 else 1    
+        self.fc_linear = nn.Linear(
+            latent_dim + num_classes, hidden_dim * np.product(out_dim[:2]),
+            bias=False)
+        self.reshape_ = (hidden_dim, *out_dim[:2])
+        self.decoder = ConvBlock(
+            dim, num_layers, hidden_dim, hidden_dim,
+            lrelu_a=kwargs.get("lrelu_a", 0.1))
+        conv_1x1 = nn.Conv2d if dim == 2 else nn.Conv1d
+        self.conv_1x1 = conv_1x1(hidden_dim, c, 1, 1, 0)
+        self.out_dim = (c, *out_dim[:2])
+
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass
+        """
+        z = self.fc_linear(z)
+        z = z.reshape(-1, *self.reshape_)
+        h = self.decoder(z)
+        h = self.conv_1x1(h)
+        h = h.reshape(-1, *self.out_dim)
+        if h.size(1) == 1:
+            h = h.squeeze(1)
+        else:
+            h = h.permute(0, 2, 3, 1)
+        return h
+
+
+class fcDecoderNet(nn.Module):
+    """
+    Decoder network (for variational autoencoder)
+
+    Args:
+        out_dim (tuple):
+            Output dimensions.
+            For images, it is (height, width) or (height, width, channels).
+            For spectra, it is (length,)
+        latent_dim (int):
+            number of latent dimensions associated with images content
+        num_layers (int):
+            number of fully connected layers
+        hidden_dim (int):
+            number of neurons in each fully connected layer
+    """
+    def __init__(self,
+                 out_dim: Tuple[int],
+                 latent_dim: int,
+                 num_layers: int = 2,
+                 hidden_dim: int = 32,
+                 num_classes: int = 0) -> None:
+        """
+        Initializes network parameters
+        """
+        super(fcDecoderNet, self).__init__()
+        if len(out_dim) not in (1, 2, 3):
+            raise ValueError(
+                "The output dimensions must be (length,) for 1D data and " +
+                "(height, width) or (height, width, channel) for 2D data")
+        c = out_dim[-1] if len(out_dim) > 2 else 1    
+        decoder = []
+        for i in range(num_layers):
+            hidden_dim_ = latent_dim + num_classes if i == 0 else hidden_dim
+            decoder.extend([nn.Linear(hidden_dim_, hidden_dim), nn.Tanh()])
+        self.decoder = nn.Sequential(*decoder)
+        self.out = nn.Linear(hidden_dim, np.product(out_dim))
+        self.out_dim = (c, *out_dim[:2])
+
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass
+        """
+        h = self.decoder(z)
+        h = self.out(h)
+        h = h.reshape(-1, *self.out_dim)
+        if h.size(1) == 1:
+            h = h.squeeze(1)
+        else:
+            h = h.permute(0, 2, 3, 1)
+        return h
+
+
+class rDecoderNet(nn.Module):
+    """
+    Spatial decoder network with skip connections
+
+    Args:
+        latent_dim (int):
+            number of latent dimensions associated with images content
+        num_layers (int):
+            number of fully connected layers
+        hidden_dim (int):
+            number of neurons in each fully connected layer
+        out_dim (tuple):
+            output dimensions: (height, width) or (height, width, channels)
+        skip (bool):
+            Use skip connections to propagate latent variables
+            through decoder network (Default: False)
+    """
+    def __init__(self,
+                 latent_dim: int,
+                 num_layers: int,
+                 hidden_dim: int,
+                 out_dim: Tuple[int],
+                 skip: bool = False,
+                 num_classes: int = 0) -> None:
+        """
+        Initializes network parameters
+        """
+        super(rDecoderNet, self).__init__()
+        if len(out_dim) == 2:
+            c = 1
+            self.reshape_ = (out_dim[0], out_dim[1])
+        else:
+            c = out_dim[-1]
+            self.reshape_ = (out_dim[0], out_dim[1], c)
+        self.skip = skip
+        self.coord_latent = coord_latent(
+            latent_dim+num_classes, hidden_dim, not skip)
+        fc_decoder = []
+        for i in range(num_layers):
+            fc_decoder.extend([nn.Linear(hidden_dim, hidden_dim), nn.Tanh()])
+        self.fc_decoder = nn.Sequential(*fc_decoder)
+        self.out = nn.Linear(hidden_dim, c)
+
+    def forward(self, x_coord: torch.Tensor, z: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass
+        """
+        batch_dim = x_coord.size()[0]
+        h = self.coord_latent(x_coord, z)
+        if self.skip:
+            residual = h
+            for i, fc_block in enumerate(self.fc_decoder):
+                h = fc_block(h)
+                if (i + 1) % 2 == 0:
+                    h = h.add(residual)
+        else:
+            h = self.fc_decoder(h)
+        h = self.out(h)
+        h = h.reshape(batch_dim, *self.reshape_)
+        return h
+
+
+class coord_latent(nn.Module):
+    """
+    The "spatial" part of the rVAE's decoder that allows for translational
+    and rotational invariance (based on https://arxiv.org/abs/1909.11663)
+
+    Args:
+        latent_dim (int):
+            number of latent dimensions associated with images content
+        out_dim (int):
+            number of output dimensions
+            (usually equal to number of hidden units
+             in the first layer of the corresponding VAE's decoder)
+        activation (bool):
+            Applies tanh activation to the output (Default: False)
+    """
+    def __init__(self,
+                 latent_dim: int,
+                 out_dim: int,
+                 activation: bool = False) -> None:
+        """
+        Initiate parameters
+        """
+        super(coord_latent, self).__init__()
+        self.fc_coord = nn.Linear(2, out_dim)
+        self.fc_latent = nn.Linear(latent_dim, out_dim, bias=False)
+        self.activation = nn.Tanh() if activation else None
+
+    def forward(self,
+                x_coord: torch.Tensor,
+                z: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass
+        """
+        batch_dim, n = x_coord.size()[:2]
+        x_coord = x_coord.reshape(batch_dim * n, -1)
+        h_x = self.fc_coord(x_coord)
+        h_x = h_x.reshape(batch_dim, n, -1)
+        h_z = self.fc_latent(z)
+        h = h_x.add(h_z.unsqueeze(1))
+        h = h.reshape(batch_dim * n, -1)
+        if self.activation is not None:
+            h = self.activation(h)
+        return h
+
 
 
 def init_imspec_model(in_dim, out_dim, latent_dim, **kwargs):
