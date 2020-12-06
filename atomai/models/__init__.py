77f8ca7b3d777de64bea1bb3b847fa1b2a26fb11
ziatdinovmax <ziatdinovmax@gmail.com>
2020-12-06 07:07:06 +0000

    Update import statements

--- atomai/models/__init__.py
+++ atomai/models/__init__.py
@@ -1,6 +1,7 @@
 from .segmentor import Segmentor
 from .imspec import ImSpec
 from .vae import BaseVAE, VAE, rVAE
-from .loaders import load_model
+from .loaders import load_model, load_ensemble
 
-__all__ = ["Segmentor", "ImSpec", "BaseVAE", "VAE", "rVAE", "load_model"]
+__all__ = ["Segmentor", "ImSpec", "BaseVAE", "VAE", "rVAE",
+           "load_model", "load_ensemble"]
