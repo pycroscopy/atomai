23149821a9fd9c695120799a56b350823d8564db
ziatdinovmax <ziatdinovmax@gmail.com>
2020-12-06 06:53:40 +0000

    Update import statements

--- atomai/__init__.py
+++ atomai/__init__.py
@@ -1,8 +1,6 @@
-from .trainers import BaseTrainer, SegTrainer, ImSpecTrainer
-from .predictors import BasePredictor, SegPredictor, ImSpecPredictor
-from . import models, trainers, predictors, nets, utils, transforms
-from .nets import load_model, load_ensemble
+from . import trainers, predictors, models, transforms, stat, utils
+from atomai.models import load_model
 from .__version__ import version as __version__
 
 __all__ = ['models', 'trainers', 'predictors', 'nets', 'utils', 'transforms',
-           'load_model', 'load_ensemble', '__version__']
+           'stat', 'load_model', '__version__']
--- atomai/models/__init__.py
+++ atomai/models/__init__.py
@@ -1,3 +1,6 @@
 from .segmentor import Segmentor
 from .imspec import ImSpec
 from .vae import BaseVAE, VAE, rVAE
+from .loaders import load_model
+
+__all__ = ["Segmentor", "ImSpec", "BaseVAE", "VAE", "rVAE", "load_model"]
