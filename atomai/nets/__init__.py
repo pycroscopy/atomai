b41fe97f798e456983fdd9caa744301a2d15c018
ziatdinovmax <ziatdinovmax@gmail.com>
2020-12-06 06:57:12 +0000

    Update import statements

--- atomai/nets/__init__.py
+++ atomai/nets/__init__.py
@@ -3,7 +3,6 @@ from .ed import (SignalDecoder, SignalED, SignalEncoder, convDecoderNet,
                  convEncoderNet, coord_latent, fcDecoderNet, fcEncoderNet,
                  init_imspec_model, rDecoderNet)
 from .fcnn import Unet, dilnet, init_fcnn_model
-from .loaders import load_ensemble, load_model
 
 __all__ = ['ConvBlock', 'UpsampleBlock', 'DilatedBlock',
            'init_fcnn_model', 'Unet', 'dilnet', 'fcEncoderNet',
