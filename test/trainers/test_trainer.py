92dfa51a6e2d51c555b9f1d322f9811779caad80
ziatdinovmax <ziatdinovmax@gmail.com>
2020-12-07 01:50:22 +0000

    Update tests

--- test/trainers/test_trainer.py
+++ test/trainers/test_trainer.py
@@ -50,24 +50,30 @@ def gen_spectra():
 def test_trainer_loss_selection(loss_user, binary, criterion_):
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_image_labels(binary)
-    t = SegTrainer(X_train, y_train, X_test, y_test,
-                   training_cycles=1, batch_size=4,
-                   loss=loss_user)
+    nb_cls = 1 if binary else 3
+    t = SegTrainer(nb_classes=nb_cls)
+    t.compile_trainer(
+        (X_train, y_train, X_test, y_test),
+        training_cycles=1, batch_size=4,
+        loss=loss_user)
     assert_equal(str(t.criterion), criterion_)
 
-@pytest.mark.parametrize("model_type", ['dilUnet', 'dilnet'])
+
+@pytest.mark.parametrize("model_type", ['Unet', 'dilnet'])
 def test_segtrainer_determinism(model_type):
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_image_labels(binary=True)
-    t1 = SegTrainer(
-        X_train, y_train, X_test, y_test, model_type=model_type,
-        training_cycles=5, batch_size=4, upsampling="nearest", seed=1)
-    t1.run()
+    t1 = SegTrainer(model_type, upsampling="nearest", seed=1)
+    t1.compile_trainer(
+        (X_train, y_train, X_test, y_test),
+        training_cycles=5, batch_size=4)
+    _ = t1.run()
     loss1 = t1.loss_acc["train_loss"][-1]
-    t2 = SegTrainer(
-        X_train, y_train, X_test, y_test, model_type=model_type,
-        training_cycles=5, batch_size=4, upsampling="nearest", seed=1)
-    t2.run()
+    t2 = SegTrainer(model_type, upsampling="nearest", seed=1)
+    t2.compile_trainer(
+        (X_train, y_train, X_test, y_test),
+        training_cycles=5, batch_size=4)
+    _ = t2.run()
     loss2 = t2.loss_acc["train_loss"][-1]
     assert_allclose(loss1, loss2)
     for p1, p2 in zip(t1.net.parameters(), t2.net.parameters()):
@@ -78,15 +84,17 @@ def test_segtrainer_determinism(model_type):
 def test_im2spec_trainer_determinism(latent_dim):
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_spectra()
-    t1 = ImSpecTrainer(X_train, y_train, X_test, y_test,
-                       latent_dim=latent_dim, training_cycles=5,
-                       batch_size=4, seed=1)
-    t1.run()
+    in_dim = (8, 8)
+    out_dim = (16,)
+    t1 = ImSpecTrainer(in_dim, out_dim, latent_dim, seed=1)
+    t1.compile_trainer((X_train, y_train, X_test, y_test),
+                       loss="mse", training_cycles=5, batch_size=4)
+    _ = t1.run()
     loss1 = t1.loss_acc["train_loss"][-1]
-    t2 = ImSpecTrainer(X_train, y_train, X_test, y_test,
-                       latent_dim=latent_dim, training_cycles=5,
-                       batch_size=4, seed=1)
-    t2.run()
+    t2 = ImSpecTrainer(in_dim, out_dim, latent_dim, seed=1)
+    t2.compile_trainer((X_train, y_train, X_test, y_test),
+                       loss="mse", training_cycles=5, batch_size=4)
+    _ = t2.run()
     loss2 = t2.loss_acc["train_loss"][-1]
     assert_allclose(loss1, loss2)
     for p1, p2 in zip(t1.net.parameters(), t2.net.parameters()):
@@ -97,14 +105,20 @@ def test_im2spec_trainer_determinism(latent_dim):
 def test_spec2im_trainer_determinism(latent_dim):
     X_train, X_test = gen_spectra()
     y_train, y_test = gen_image_data()
-    t1 = ImSpecTrainer(X_train, y_train, X_test, y_test,
-                       latent_dim=latent_dim, training_cycles=5,
-                       batch_size=4, seed=1)
-    t1.run()
+    in_dim = (16,)
+    out_dim = (8, 8)
+    t1 = ImSpecTrainer(
+        in_dim, out_dim, latent_dim, seed=1)
+    t1.compile_trainer(
+        (X_train, y_train, X_test, y_test),
+        loss="mse", training_cycles=5, batch_size=4)
+    _ = t1.run()
     loss1 = t1.loss_acc["train_loss"][-1]
-    t2 = ImSpecTrainer(X_train, y_train, X_test, y_test,
-                       latent_dim=latent_dim, training_cycles=5,
-                       batch_size=4, seed=1)
+    t2 = ImSpecTrainer(
+        in_dim, out_dim, latent_dim, seed=1)
+    t2.compile_trainer(
+        (X_train, y_train, X_test, y_test),
+        loss="mse", training_cycles=5, batch_size=4)
     t2.run()
     loss2 = t2.loss_acc["train_loss"][-1]
     assert_allclose(loss1, loss2)
@@ -118,8 +132,11 @@ def test_spec2im_trainer_determinism(latent_dim):
 def test_segtrainer_dataloader(binary, tensor_type):
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_image_labels(binary)
-    t = SegTrainer(
-        X_train, y_train, X_test, y_test, training_cycles=1, batch_size=4)
+    nb_cls = 1 if binary else 3
+    t = SegTrainer(nb_classes=nb_cls)
+    t.compile_trainer(
+        (X_train, y_train, X_test, y_test),
+        training_cycles=1, batch_size=4)
     X_train_, y_train_ = t.dataloader(0)
     assert_equal(X_train_.dtype, torch.float32)
     assert_equal(y_train_.dtype, tensor_type)
@@ -130,8 +147,10 @@ def test_segtrainer_dataloader(binary, tensor_type):
 def test_im2spectrainer_dataloader():
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_spectra()
-    t = ImSpecTrainer(
-        X_train, y_train, X_test, y_test, training_cycles=1, batch_size=4)
+    in_dim, out_dim = (8, 8), (16,)
+    t = ImSpecTrainer(in_dim, out_dim, seed=1)
+    t.compile_trainer((X_train, y_train, X_test, y_test),
+                      loss="mse", training_cycles=5, batch_size=4)
     X_train_, y_train_ = t.dataloader(0)
     assert_equal(X_train_.dtype, torch.float32)
     assert_equal(y_train_.dtype, torch.float32)
@@ -142,8 +161,10 @@ def test_im2spectrainer_dataloader():
 def test_spec2imtrainer_dataloader():
     X_train, X_test = gen_spectra()
     y_train, y_test = gen_image_data()
-    t = ImSpecTrainer(
-        X_train, y_train, X_test, y_test, training_cycles=1, batch_size=4)
+    in_dim, out_dim = (16,), (8, 8)
+    t = ImSpecTrainer(in_dim, out_dim, seed=1)
+    t.compile_trainer((X_train, y_train, X_test, y_test),
+                      loss="mse", training_cycles=5, batch_size=4)
     X_train_, y_train_ = t.dataloader(0)
     assert_equal(X_train_.dtype, torch.float32)
     assert_equal(y_train_.dtype, torch.float32)
@@ -159,9 +180,10 @@ def test_spec2imtrainer_dataloader():
 def test_init_unet_bn(bn, n_bn, layers):
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_image_labels(binary=False)
-    t = SegTrainer(X_train, y_train, X_test, y_test,
-                   model="dilUnet", use_batchnorm=bn,
-                   layers=layers, batch_size=4)
+    t = SegTrainer("Unet", nb_classes=3, batch_norm=bn, layers=layers)
+    t.compile_trainer(
+        (X_train, y_train, X_test, y_test),
+        training_cycles=1, batch_size=4)
     n_bn_ = len([k for k in t.net.state_dict().keys() if 'running_mean' in k])
     assert_equal(n_bn_, n_bn)
 
@@ -172,9 +194,10 @@ def test_init_unet_bn(bn, n_bn, layers):
 def test_init_unet_dropouts(dropouts, n_dropouts):
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_image_labels(binary=False)
-    t = SegTrainer(X_train, y_train, X_test, y_test,
-                   model="dilUnet", use_dropouts=dropouts,
-                   batch_size=4)
+    t = SegTrainer("Unet", nb_classes=3, dropout=dropouts)
+    t.compile_trainer(
+        (X_train, y_train, X_test, y_test),
+        training_cycles=1, batch_size=4)
     n_dropouts_ = 0
     for c in t.net.children():
         if 'Dropout' in str([m for m in c.named_modules()]):
@@ -187,9 +210,10 @@ def test_init_unet_dropouts(dropouts, n_dropouts):
 def test_init_unet_layers(layers):
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_image_labels(binary=False)
-    t = SegTrainer(X_train, y_train, X_test, y_test,
-                   model="dilUnet", layers=layers,
-                   batch_size=4)
+    t = SegTrainer("Unet", nb_classes=3, layers=layers)
+    t.compile_trainer(
+        (X_train, y_train, X_test, y_test),
+        training_cycles=1, batch_size=4)
     n_bn_ = len([k for k in t.net.state_dict().keys() if 'running_mean' in k])
     layers_ = len([k for k in t.net.state_dict().keys() if "weight" in k])
     layers_ = layers_ - 4 - n_bn_
@@ -204,9 +228,11 @@ def test_init_unet_layers(layers):
 def test_init_dilnet_bn(bn, n_bn, layers):
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_image_labels(binary=False)
-    t = SegTrainer(X_train, y_train, X_test, y_test,
-                   model="dilnet", use_batchnorm=bn,
-                   layers=layers, batch_size=4)
+    t = SegTrainer(
+        "dilnet", nb_classes=3, batch_norm=bn, layers=layers)
+    t.compile_trainer(
+        (X_train, y_train, X_test, y_test),
+        training_cycles=1, batch_size=4)
     n_bn_ = len([k for k in t.net.state_dict().keys() if 'running_mean' in k])
     assert_equal(n_bn_, n_bn)
 
@@ -217,9 +243,11 @@ def test_init_dilnet_bn(bn, n_bn, layers):
 def test_init_dilnet_dropouts(dropouts, n_dropouts):
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_image_labels(binary=False)
-    t = SegTrainer(X_train, y_train, X_test, y_test,
-                   model="dilnet", use_dropouts=dropouts,
-                   batch_size=4)
+    t = SegTrainer(
+        "dilnet", nb_classes=3, dropout=dropouts)
+    t.compile_trainer(
+        (X_train, y_train, X_test, y_test),
+        training_cycles=1, batch_size=4)
     n_dropouts_ = 0
     for c in t.net.children():
         if 'Dropout' in str([m for m in c.named_modules()]):
@@ -232,8 +260,10 @@ def test_init_dilnet_dropouts(dropouts, n_dropouts):
 def test_init_dilnet_layers(layers):
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_image_labels(binary=False)
-    t = SegTrainer(X_train, y_train, X_test, y_test,
-                   model="dilnet", layers=layers, batch_size=4)
+    t = SegTrainer("dilnet", nb_classes=3, layers=layers)
+    t.compile_trainer(
+        (X_train, y_train, X_test, y_test),
+        training_cycles=1, batch_size=4)
     n_bn_ = len([k for k in t.net.state_dict().keys() if 'running_mean' in k])
     layers_ = len([k for k in t.net.state_dict().keys() if "weight" in k])
     layers_ = layers_ - 2 - n_bn_
@@ -242,18 +272,21 @@ def test_init_dilnet_layers(layers):
 
 @pytest.mark.parametrize(
     "model, nb_filters_in, nb_filters_layers",
-    [("dilUnet", 25, [25, 50, 100, 200, 100, 50, 25]),
+    [("Unet", 25, [25, 50, 100, 200, 100, 50, 25]),
      ("dilnet", 25, [25, 50, 50, 25])])
 def test_init_segmodel_filters(model, nb_filters_in, nb_filters_layers):
-    from atomai.nets.blocks import upsample_block
+    from atomai.nets.blocks import UpsampleBlock
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_image_labels(binary=False)
-    t = SegTrainer(X_train, y_train, X_test, y_test,
-                   model=model, nb_filters=nb_filters_in,
-                   use_batchnorm=False, batch_size=4)
+    t = SegTrainer(
+        model, nb_classes=3,
+        batch_norm=False, nb_filters=nb_filters_in)
+    t.compile_trainer(
+        (X_train, y_train, X_test, y_test),
+        training_cycles=1, batch_size=4)
     filters_ = []
     for child in t.net.children():
-        if isinstance(child, upsample_block):
+        if isinstance(child, UpsampleBlock):
             continue
         filt_ = []
         for p in child.state_dict().values():
@@ -268,10 +301,11 @@ def test_init_segmodel_filters(model, nb_filters_in, nb_filters_layers):
 def test_init_imspec_bn(bn, encoder_layers, decoder_layers, n_bn):
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_spectra()
-    t = ImSpecTrainer(X_train, y_train, X_test, y_test,
-                      nblayers_encoder=encoder_layers,
-                      nblayers_decoder=decoder_layers,
-                      use_batchnorm=bn, batch_size=4, training_cycles=1)
+    in_dim, out_dim = (8, 8), (16,)
+    t = ImSpecTrainer(in_dim, out_dim, nblayers_encoder=encoder_layers,
+                      nblayers_decoder=decoder_layers, batch_norm=bn)
+    t.compile_trainer((X_train, y_train, X_test, y_test),
+                      batch_size=4, loss="mse", training_cycles=1)
     n_bn_ = len([k for k in t.net.state_dict().keys() if 'running_mean' in k])
     assert_equal(n_bn_, n_bn)
 
@@ -283,9 +317,10 @@ def test_init_im2spec_edownsample(downsample_factor, output_dim):
     from atomai.utils import Hook, mock_forward
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_spectra()
-    t = ImSpecTrainer(X_train, y_train, X_test, y_test,
-                      encoder_downsampling=downsample_factor,
-                      batch_size=4, training_cycles=1)
+    in_dim, out_dim = (8, 8), (16,)
+    t = ImSpecTrainer(in_dim, out_dim, encoder_downsampling=downsample_factor)
+    t.compile_trainer((X_train, y_train, X_test, y_test),
+                      batch_size=4, loss="mse", training_cycles=1)
     hookF = [Hook(layer[1]) for layer in list(t.net.encoder._modules.items())]
     mock_forward(t.net, dims=(1, 8, 8))
     assert_equal(tuple([h.output.shape[-2:] for h in hookF][0]), output_dim)
@@ -298,9 +333,10 @@ def test_init_spec2im_edownsample(downsample_factor, output_dim):
     from atomai.utils import Hook, mock_forward
     X_train, X_test = gen_spectra()
     y_train, y_test = gen_image_data()
-    t = ImSpecTrainer(X_train, y_train, X_test, y_test,
-                      encoder_downsampling=downsample_factor,
-                      batch_size=4, training_cycles=1)
+    in_dim, out_dim = (16,), (8, 8)
+    t = ImSpecTrainer(in_dim, out_dim, encoder_downsampling=downsample_factor)
+    t.compile_trainer((X_train, y_train, X_test, y_test),
+                      batch_size=4, loss="mse", training_cycles=1)
     hookF = [Hook(layer[1]) for layer in list(t.net.encoder._modules.items())]
     mock_forward(t.net, dims=(1, 16))
     assert_equal([h.output.shape[-1] for h in hookF][0], output_dim)
@@ -312,9 +348,10 @@ def test_init_im2spec_dupsample(upsample, dim_):
     from atomai.utils import Hook, mock_forward
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_spectra()
-    t = ImSpecTrainer(X_train, y_train, X_test, y_test,
-                      decoder_upsampling=upsample,
-                      batch_size=4, training_cycles=1)
+    in_dim, out_dim = (8, 8), (16,)
+    t = ImSpecTrainer(in_dim, out_dim, decoder_upsampling=upsample)
+    t.compile_trainer((X_train, y_train, X_test, y_test),
+                      batch_size=4, loss="mse", training_cycles=1)
     hookF = [Hook(layer[1]) for layer in list(t.net.decoder._modules.items())]
     mock_forward(t.net, dims=(1, 8, 8))
     assert_equal([h.output.shape[-1] for h in hookF][1:4], dim_)
@@ -326,9 +363,10 @@ def test_init_spec2im_dupsample(upsample, dim_):
     from atomai.utils import Hook, mock_forward
     X_train, X_test = gen_spectra()
     y_train, y_test = gen_image_data()
-    t = ImSpecTrainer(X_train, y_train, X_test, y_test,
-                      decoder_upsampling=upsample,
-                      batch_size=4, training_cycles=1)
+    in_dim, out_dim = (16,), (8, 8)
+    t = ImSpecTrainer(in_dim, out_dim, decoder_upsampling=upsample)
+    t.compile_trainer((X_train, y_train, X_test, y_test),
+                      batch_size=4, loss="mse", training_cycles=1)
     hookF = [Hook(layer[1]) for layer in list(t.net.decoder._modules.items())]
     mock_forward(t.net, dims=(1, 16))
     assert_equal([h.output.shape[-1] for h in hookF][1:4], dim_)
@@ -338,21 +376,23 @@ def test_init_spec2im_dupsample(upsample, dim_):
     "efilt, dfilt, filters_e_sorted, filters_d_sorted",
     [(25, 50, [25], [1, 50]), (32, 16, [32], [1, 16])])
 def test_im2spec_filters(efilt, dfilt, filters_e_sorted, filters_d_sorted):
-    from atomai.nets import signal_decoder, signal_encoder
+    from atomai.nets import SignalDecoder, SignalEncoder
     X_train, X_test = gen_image_data()
     y_train, y_test = gen_spectra()
-    t = ImSpecTrainer(X_train, y_train, X_test, y_test,
-                      nbfilters_encoder=efilt, nbfilters_decoder=dfilt,
-                      batch_size=4, training_cycles=1)
+    in_dim, out_dim = (8, 8), (16,)
+    t = ImSpecTrainer(
+        in_dim, out_dim, nbfilters_encoder=efilt, nbfilters_decoder=dfilt)
+    t.compile_trainer((X_train, y_train, X_test, y_test),
+                      batch_size=4, loss="mse", training_cycles=1)
     filters_e, filters_d = [], []
     for child in t.net.children():
-        if isinstance(child, signal_encoder):
+        if isinstance(child, SignalEncoder):
             filt_ = []
             for p in child.state_dict().values():
                 if len(p.shape) == 4:
                     filt_.append(p.shape[0])
             filters_e.extend(filt_)
-        elif isinstance(child, signal_decoder):
+        elif isinstance(child, SignalDecoder):
             filt_ = []
             for p in child.state_dict().values():
                 if len(p.shape) == 3:
@@ -366,21 +406,23 @@ def test_im2spec_filters(efilt, dfilt, filters_e_sorted, filters_d_sorted):
     "efilt, dfilt, filters_e_sorted, filters_d_sorted",
     [(25, 50, [25], [1, 50]), (32, 16, [32], [1, 16])])
 def test_spec2im_filters(efilt, dfilt, filters_e_sorted, filters_d_sorted):
-    from atomai.nets import signal_decoder, signal_encoder
+    from atomai.nets import SignalDecoder, SignalEncoder
     X_train, X_test = gen_spectra()
     y_train, y_test = gen_image_data()
-    t = ImSpecTrainer(X_train, y_train, X_test, y_test,
-                      nbfilters_encoder=efilt, nbfilters_decoder=dfilt,
-                      batch_size=4, training_cycles=1)
+    in_dim, out_dim = (16,), (8, 8)
+    t = ImSpecTrainer(
+        in_dim, out_dim, nbfilters_encoder=efilt, nbfilters_decoder=dfilt)
+    t.compile_trainer((X_train, y_train, X_test, y_test),
+                      batch_size=4, loss="mse", training_cycles=1)
     filters_e, filters_d = [], []
     for child in t.net.children():
-        if isinstance(child, signal_encoder):
+        if isinstance(child, SignalEncoder):
             filt_ = []
             for p in child.state_dict().values():
                 if len(p.shape) == 3:
                     filt_.append(p.shape[0])
             filters_e.extend(filt_)
-        elif isinstance(child, signal_decoder):
+        elif isinstance(child, SignalDecoder):
             filt_ = []
             for p in child.state_dict().values():
                 if len(p.shape) == 4:
