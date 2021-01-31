153f4a741fc4cfa9f2186cbe8afcf6f5b86b72e3
ziatdinovmax <ziatdinovmax@gmail.com>
2021-01-31 04:12:32 +0000

    Move loss functions for VAE to a spearate module

--- atomai/losses_metrics/__init__.py
+++ atomai/losses_metrics/__init__.py
@@ -1,6 +1,8 @@
-from .losses import (dice_loss, focal_loss, joint_vae_loss, rvae_loss,
-                     select_loss, vae_loss, joint_rvae_loss)
+from .losses import dice_loss, focal_loss, select_loss
 from .metrics import IoU
+from .vi_losses import (joint_rvae_loss, joint_vae_loss, kld_discrete,
+                        kld_normal, kld_rot, rvae_loss, vae_loss)
 
 __all__ = ['focal_loss', 'dice_loss', 'select_loss', "vae_loss",
-           "rvae_loss", "joint_vae_loss", "joint_rvae_loss", "IoU"]
+           "rvae_loss", "joint_vae_loss", "joint_rvae_loss", "IoU",
+           "kld_normal", "kld_discrete", "kld_rot"]
