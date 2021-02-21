d8420c14095808278e17e906df0adb1a5bdc850b
ziatdinovmax <ziatdinovmax@gmail.com>
2021-02-21 03:53:39 +0000

    Split vae module into vae, tvae, jvae, and jrvae

--- atomai/models/dgm/__init__.py
+++ atomai/models/dgm/__init__.py
@@ -1,4 +1,7 @@
-from .vae import BaseVAE, VAE, rVAE, jVAE, jrVAE
+from .vae import BaseVAE, VAE
+from .rvae import rVAE
+from .jvae import jVAE
+from .jrvae import jrVAE
 from .aux_vae import ssVAE
 
 __all__ = ["BaseVAE", "VAE", "rVAE", "jVAE", "jrVAE", "ssVAE"]
