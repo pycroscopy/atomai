c21872b1914b3d052af0e26d360626423be2ee31
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2021-05-31 06:02:31 +0000

    Add datasets module

---
+++ atomai/utils/datasets.py
@@ -0,0 +1,40 @@
+import os
+import urllib.request
+
+import numpy as np
+import progressbar
+
+
+def stem_smbfo(filedir: str = './'):
+    """
+    Downloads the scanning transmission electron microscopy (STEM) datasets
+    from the combinatorial library of the Sm-doped BiFeO3 (BFO) grown to cover
+    the composition range from pure ferroelectric BFO to orthorhombic 20% Sm-doped BFO
+
+    Args:
+        filedir: directory to save the downloaded data
+    """
+    print("If you use this dataset in your work, please consider citing it"
+          " using the following DOI: https://doi.org/10.13139/ORNLNCCS/1773704")
+    url = "https://zenodo.org/record/4876786/files/composition_series_dict_full.npy"
+    urllib.request.urlretrieve(
+        url, os.path.join(filedir, "SmBFO_composition_series.npy"), ProgressBar())
+    dataset = np.load(
+        os.path.join(filedir, "SmBFO_composition_series.npy"), allow_pickle=True)[()]
+    return dataset
+
+
+class ProgressBar():
+    def __init__(self):
+        self.pbar = None
+
+    def __call__(self, block_num, block_size, total_size):
+        if not self.pbar:
+            self.pbar = progressbar.ProgressBar(maxval=total_size)
+            self.pbar.start()
+
+        downloaded = block_num * block_size
+        if downloaded < total_size:
+            self.pbar.update(downloaded)
+        else:
+            self.pbar.finish()
