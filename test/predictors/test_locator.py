433a09f06edddb0c5051930b710e589e53dc26e3
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2021-06-07 05:55:35 +0000

    Update tests

--- test/predictors/test_locator.py
+++ test/predictors/test_locator.py
@@ -10,6 +10,8 @@ from atomai.predictors import Locator
 
 test_nnout = np.load(os.path.join(
     os.path.dirname(__file__), 'test_data/test_nnoutput.npy'))
+test_inimg = np.load(os.path.join(
+    os.path.dirname(__file__), 'test_data/test_inputimg.npy'))
 test_coord = np.load(os.path.join(
     os.path.dirname(__file__), 'test_data/test_coordinates.npy'),
     allow_pickle=True)[()]
@@ -18,7 +20,7 @@ test_coord = np.load(os.path.join(
 def test_locator():
     loc = Locator()
     coord = loc.run(test_nnout)
-    assert_allclose(test_coord[0], coord[0])
+    assert_(np.array_equal(test_coord[0], coord[0]))
 
 
 def test_locator_edge():
@@ -34,4 +36,14 @@ def test_locator_dimorder():
     nnout = test_nnout.transpose(0, -1, 1, 2)
     loc = Locator(dim_order='channel_first')
     coord = loc.run(nnout)
-    assert_allclose(test_coord[0], coord[0])
+    assert_(np.array_equal(test_coord[0], coord[0]))
+
+
+def test_locator_refine():
+    nnout = test_nnout[:, 0:256, 0:256]
+    inimg = test_inimg[0:256, 0:256][None, ..., None]
+    loc = Locator()
+    coord1 = loc.run(nnout)
+    loc = Locator(refine=True)
+    coord2 = loc.run(nnout, inimg)
+    assert(not np.array_equal(coord1[0], coord2[0]))
