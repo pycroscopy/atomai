7f8ddbdf91e0a92d8d6dd2c7827c6b881d8be1c5
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2023-03-29 16:20:42 -0400

    Update imports

--- atomai/predictors/__init__.py
+++ atomai/predictors/__init__.py
@@ -1,5 +1,5 @@
-from .predictor import BasePredictor, SegPredictor, ImSpecPredictor, Locator
+from .predictor import BasePredictor, SegPredictor, ImSpecPredictor, RegPredictor, Locator
 from .epredictor import EnsemblePredictor, ensemble_locate
 
-__all__ = ["BasePredictor", "SegPredictor", "ImSpecPredictor",
+__all__ = ["BasePredictor", "SegPredictor", "ImSpecPredictor", "RegPredictor",
            "EnsemblePredictor", "ensemble_locate", "Locator"]
