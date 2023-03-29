ac45e5a5d635ea81e433c61158a06e82abbedbe9
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2023-03-29 17:12:44 -0400

    Update imports

--- atomai/models/__init__.py
+++ atomai/models/__init__.py
@@ -1,9 +1,10 @@
 from .segmentor import Segmentor
 from .imspec import ImSpec
+from.regressor import Regressor
 from .dgm import BaseVAE, VAE, rVAE, jVAE, jrVAE
 from .dklgp import dklGPR
 from .loaders import load_model, load_ensemble, load_pretrained_model
 
 __all__ = ["Segmentor", "ImSpec", "BaseVAE", "VAE", "rVAE",
            "jVAE", "jrVAE", "load_model", "load_ensemble",
-           "load_pretrained_model", "dklGPR"]
+           "load_pretrained_model", "dklGPR", "Regressor"]
