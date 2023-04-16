9ce9650a093181dfc4002c4878fd2648ad9d1e2b
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2023-04-15 23:43:54 -0400

    Update reconstructor class

--- atomai/models/dklgp/gpr.py
+++ atomai/models/dklgp/gpr.py
@@ -15,9 +15,9 @@ class Reconstructor(GPTrainer):
 
     def fit(self, X: torch.Tensor, y: torch.Tensor,
             training_cycles: int, **kwargs):
-        self.run(X, y, training_cycles, **kwargs)
+        _ = self.run(X, y, training_cycles, **kwargs)
 
-    def predict(self, X_new: torch.Tensor, img_shape: Tuple[int], **kwargs):
+    def predict(self, X_new: torch.Tensor, **kwargs):
         batch_size = kwargs.get("batch_size", len(X_new))
         X_new_batches = create_batches(X_new, batch_size)
         self.gp_model.eval()
@@ -28,11 +28,11 @@ class Reconstructor(GPTrainer):
                 self._set_data(x, **kwargs)
                 y_pred = self.likelihood(self.gp_model(x))
                 reconstruction.append(y_pred)
-        reconstruction = torch.cat(reconstruction)
-        return reconstruction.mean.view(img_shape).cpu().numpy()
+        return torch.cat(reconstruction)
 
     def reconstruct(self, sparse_image: np.ndarray,
                     training_cycles: int = 100, **kwargs):
         X_train, y_train, X_full = prepare_gp_input(sparse_image)
         self.fit(X_train, y_train, training_cycles, **kwargs)
-        return self.predict(X_full, sparse_image.shape, **kwargs)
+        reconstruction = self.predict(X_full, **kwargs)
+        return reconstruction.mean.view(sparse_image.shape).cpu().numpy()
