7c44256c260f7b4b769f5be2a9737c5f50e2024b
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2025-06-04 22:51:38 -0700

    Add basic image denoiser

--- atomai/models/__init__.py
+++ atomai/models/__init__.py
@@ -2,6 +2,7 @@ from .segmentor import Segmentor
 from .imspec import ImSpec
 from .regressor import Regressor
 from .classifier import Classifier
+from .denoiser import DenoisingAutoencoder, denoise_images
 from .dgm import BaseVAE, VAE, rVAE, jVAE, jrVAE
 from .dklgp import dklGPR, Reconstructor
 from .loaders import load_model, load_ensemble, load_pretrained_model
@@ -9,4 +10,5 @@ from .loaders import load_model, load_ensemble, load_pretrained_model
 __all__ = ["Segmentor", "ImSpec", "BaseVAE", "VAE", "rVAE",
            "jVAE", "jrVAE", "load_model", "load_ensemble",
            "load_pretrained_model", "dklGPR", "Regressor",
-           "Classifier", "Reconstructor"]
+           "Classifier", "Reconstructor", "DenoisingAutoencoder",
+           "denoise_images"]
